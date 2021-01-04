4406791de67208a589cf3a1ac0946a4601c06986
jiho lee <optional.int@kakao.com>
2021-01-04 18:05:43 +0900

    Initial commit

---
+++ src/main/java/io/github/dps0340/springbootaws/SpringbootAwsApplication.java
@@ -0,0 +1,13 @@
+package io.github.dps0340.springbootaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/dps0340/springbootaws/SpringbootAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dps0340.springbootaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
